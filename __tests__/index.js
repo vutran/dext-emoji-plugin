fac44d759be058e66b0f59fa5ee7ea6f4a8f0f8d
Tormen Raßmann <tormen.rassmann@eatfirst.com>
2017-11-04 02:05:37 +0100

    #2 Switch to emojilib (#3)
    * #2 Switch to emojilib
    
    * #2 Remove unused mock, remove unneeded test

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,38 +1,13 @@
 import m from '../';
 
-jest.mock('got');
-
 describe('dext-emoji-plugin', () => {
-  // eslint-disable-next-line global-require, no-underscore-dangle
-  require('got').__setFakeData({
-    results: [
-      {
-        text: '🍕',
-        score: 0.6858612895,
-      },
-    ],
-  });
-
   it('should retrieve some results from the api', async () => {
     const results = await m.query('pizza');
     expect(results.items.length).toBe(1);
   });
 
   it('should not return any results', async () => {
-    // eslint-disable-next-line global-require, no-underscore-dangle
-    require('got').__setFakeData({
-      results: [],
-    });
-
-    const results = await m.query('bar');
-    expect(results.items.length).toBe(0);
-  });
-
-  it('should handle a rejected Promise', async () => {
-    // eslint-disable-next-line global-require, no-underscore-dangle
-    require('got').__setReject(true);
-
-    const results = await m.query('bar');
+    const results = await m.query('ranch');
     expect(results.items.length).toBe(0);
   });
 });
